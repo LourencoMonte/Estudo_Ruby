1e634df7bc2d1ca48027781a5f0843c447a64798
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-02-14 18:25:09 -0300

    Adicionando arquivo de exercicios práticos

---
+++ Estudo_ruby/generalExercising.rb
@@ -0,0 +1,25 @@
+    #Parte 1
+=begin
+puts "Escreva um número"
+numero1 = gets.chomp.to_i
+
+puts "Escreva outro número"
+numero2 = gets.chomp.to_i
+
+puts " A soma dos número #{numero1} e #{numero2} é: #{numero1 + numero2}"
+=end
+
+    #Parte 2
+=begin
+puts "Digite sua idade"
+age = gets.chomp.to_i
+
+if age >= 18
+    puts "Você é maior de idade" 
+
+else
+    puts "Você é menor de idade"
+end
+=end
+
+    #Parte 3
