61c86c6f776f909e62ef192f0c0abe06691f7df0
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-02-14 19:17:13 -0300

    Inicializado Exercicio 1, referenta a atividades acadêmicas

---
+++ Estudo_Java_POO/VS_Code/estruturasDeDados/e1/Questao.java
@@ -0,0 +1,55 @@
+package estruturasDeDados.e1;
+
+public class Questao {
+    //Atributos
+    private String enunciado; 
+    private String[] alternativas; 
+    private int indiceCorreto;
+ 
+    //Construtor
+    public Questao(String enunciado, String [] alternativas, int indiceCorreto){
+        this.enunciado = enunciado;
+        this.alternativas = alternativas;
+        this.indiceCorreto = indiceCorreto;
+    }
+
+    //Getters e Setters
+    public String getEnunciado(){
+        return enunciado;
+    }
+    public void setEnunciado(String enunciado){
+        this.enunciado = enunciado;
+    }
+
+    public String[] getAlternativas(){
+        return alternativas;
+    }
+    public void setAlternativas(String[] alternativas){
+        this.alternativas = alternativas;
+    }
+
+    public int getIndiceCorreto(){
+        return indiceCorreto;
+    }
+    public void setIndiceCorreto(int indiceCorreto){
+        this.indiceCorreto = indiceCorreto;
+    }
+}
+
+
+//Construir a classe Prova
+    candidato
+    4 questõess
+    4 respostas
+    nota
+
+//Método aplicar prova
+    mostrar questão
+        ler e guardar resposta
+
+//Método corrigir prova
+    nota = 0
+    if correto + 2,5f
+    exibir resultado
+
+//Construir classe teste
