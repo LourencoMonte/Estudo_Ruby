eb2206c6fbeeef01609f3b3a7c6f4ac7fd671b4b
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-02-20 08:02:10 -0300

    refactor: encapsula codigo de verificação de aprovação de aluno em método

--- Estudo_ruby/loresGem1/lib/loresGem1/if_elsif_else.rb
+++ Estudo_ruby/loresGem1/lib/loresGem1/if_elsif_else.rb
@@ -1,10 +1,15 @@
-puts "Digite a nota do aluno."
-nota = gets.chomp.to_f
+def verificar_aprovacao_aluno
+    puts "Digite a nota do aluno."
+    nota = gets.chomp.to_f
 
-if nota < 5
-    puts "O aluno está reprovado. Mais empenho na próxima vez."
-elsif nota >= 5 && nota < 7
-    puts "O aluno está de recuperação. Se empenhe para passar."
-else
-    puts "Parabéns! Seus esforços serão reconhecidos."
+    if nota < 5
+        puts "O aluno está reprovado. Mais empenho na próxima vez."
+    elsif nota >= 5 && nota < 7
+        puts "O aluno está de recuperação. Se empenhe para passar."
+    else
+        puts "Parabéns! Seus esforços serão reconhecidos."
+    end
 end
+
+#Chamada
+verificar_aprovacao_aluno
