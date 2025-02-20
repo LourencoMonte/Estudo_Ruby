ef28f5bb8f36bad67f659f9b4c32023d14ce7889
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-02-20 08:06:27 -0300

    refactor: encapsula código de verificação de senha em método

--- Estudo_ruby/loresGem1/lib/loresGem1/until.rb
+++ Estudo_ruby/loresGem1/lib/loresGem1/until.rb
@@ -1,9 +1,14 @@
-password = nil
-until password == 1234
-    puts "Digite a senha"
-    password = gets.chomp.to_i
-        if password != 1234
-            puts "Senha incorreta"
-        end 
+def verificar_senha
+    password = nil
+    until password == 1234
+        puts "Digite a senha"
+        password = gets.chomp.to_i
+            if password != 1234
+                puts "Senha incorreta"
+            end 
+    end
+    puts "Senha correta"
 end
-puts "Senha correta"
+
+#Chamada de método
+verificar_senha
