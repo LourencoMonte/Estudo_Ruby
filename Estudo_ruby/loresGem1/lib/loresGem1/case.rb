2d56beab0af21c76f234c2409a5a361009f7e7c2
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-02-20 07:58:46 -0300

    refactor: encapsulamento de código de verificação de dia da semana em método

--- Estudo_ruby/loresGem1/lib/loresGem1/case.rb
+++ Estudo_ruby/loresGem1/lib/loresGem1/case.rb
@@ -1,22 +1,27 @@
-loop do
-puts "Selecione uma opção de 1 a 7"
-puts "(1). Domingo"
-puts "(2). Segunda-feira"
-puts "(3). Terça-feira"
-puts "(4). Quarta-feira"
-puts "(5). Quinta-feira"
-puts "(6). Sexta-feira"
-puts "(7). Sábado"
-day = gets.chomp.to_i
+def verificar_dia_da_semana
+    loop do
+    puts "Selecione uma opção de 1 a 7"
+    puts "(1). Domingo"
+    puts "(2). Segunda-feira"
+    puts "(3). Terça-feira"
+    puts "(4). Quarta-feira"
+    puts "(5). Quinta-feira"
+    puts "(6). Sexta-feira"
+    puts "(7). Sábado"
+    day = gets.chomp.to_i
 
-case day
-when 1, 7
-    puts "É final de semana"
-    break
-when 2..6
-    puts "É dia útil"
-    break
-else
-    puts "Opção inválida. Por gentileza, selecione um número de 1 a 7."
-end 
+        case day
+        when 1, 7
+            puts "É final de semana"
+            break
+        when 2..6
+            puts "É dia útil"
+            break
+        else
+            puts "Opção inválida. Por gentileza, selecione um número de 1 a 7."
+        end 
+    end
 end
+
+#Chamada
+verificar_dia_da_semana
