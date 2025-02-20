02b3852b41a29b3831b12ad5676dea87801923cd
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-02-20 16:52:09 -0300

    refactor: refatorando código de tabuda, adicionando classe e  métodos

--- Estudo_ruby/loresGem1/lib/loresGem1/while.rb
+++ Estudo_ruby/loresGem1/lib/loresGem1/while.rb
@@ -1,21 +1,39 @@
-#Tabuada de soma
-count = 0
-
-puts "Digite um número e veja a sua tabuada."
-number = gets.chomp.to_i
-
-puts "Tabuada de SOMA de #{number}"
-while count <= 10
-    puts "#{number} + #{count} = #{number + count}"
-    count += 1
-end
-
-#Tabuada de subtração
-puts
-count = 1
-
-puts "Tabuada de SUBTRAÇÃO de #{number}"
-while count <= 10
-    puts "#{number + count} - #{number} = #{count}"
-    count += 1
-end
+class Tabuada
+    def initialize(number)
+      @number = number
+    end
+  
+    def tabuada_soma
+      count = 0
+      result = []
+      while count <= 10
+        result << "#{@number} + #{count} = #{@number + count}"
+        count += 1
+      end
+      result
+    end
+  
+    def tabuada_subtracao
+      count = 1
+      result = []
+      while count <= 10
+        result << "#{@number + count} - #{@number} = #{count}"
+        count += 1
+      end
+      result
+    end
+  
+    def mostrar_tabuadas
+      puts "Tabuada de SOMA de #{@number}"
+      puts tabuada_soma
+      puts "\nTabuada de SUBTRAÇÃO de #{@number}"
+      puts tabuada_subtracao
+    end
+  end
+  
+  # Chamada do programa
+  puts "Digite um número e veja a sua tabuada."
+  number = gets.chomp.to_i
+  tabuada = Tabuada.new(number)
+  tabuada.mostrar_tabuadas
+  
