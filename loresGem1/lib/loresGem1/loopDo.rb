9c36a40bbcf1d0e7809b68a9d64d659159f5bcbc
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-03-04 23:11:27 -0300

    feat: adiciona arquivo de estudo sobre loop do

---
+++ loresGem1/lib/loresGem1/loopDo.rb
@@ -0,0 +1,27 @@
+count = 0
+
+loop do
+    puts "Número #{count}"
+    count += 1
+    break if count == 10
+end
+
+number = nil
+loop do
+    puts "Digite um número pá."
+    number = gets.chomp.to_i
+    break if number % 2 == 0
+
+    puts "Número inválido. Digite um número pá"
+end
+
+puts "Número válido: #{number}"
+
+num = 0
+
+loop do
+    num += 1
+    next if num.even?
+    puts num
+    break if num >= 10
+end
