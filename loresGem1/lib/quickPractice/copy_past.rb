c1ef9d2625beee837379c5359049557653a50882
LourencoMonte <f.lourenco13aps@gmail.com>
2025-03-19 15:21:51 -0300

    Adionado itens teste criados por algoritmo manipulador de dados

---
+++ loresGem1/lib/quickPractice/copy_past.rb
@@ -0,0 +1,6 @@
+puts "Digite o número correspondente ao arquivo que vai ser copiado:"
+puts '[1] arquivo_misto.txt'
+puts '[2] novo_arquivo.txt'
+puts '[3] exemplo.txt'
+file_name = gets.chomp.to_i
+
