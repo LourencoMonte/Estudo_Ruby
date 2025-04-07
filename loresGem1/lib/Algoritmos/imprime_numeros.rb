89174d76a0034f30a1d3219a5747bf67b6b3b127
LourencoMonte <f.lourenco13aps@gmail.com>
2025-04-07 13:25:38 -0300

    feat: Adiciona algoritmo que imprime em arrays diferentes números impares e pares

---
+++ loresGem1/lib/Algoritmos/imprime_numeros.rb
@@ -0,0 +1,17 @@
+impares = []
+pares = []
+
+puts 'Digite  4 números'
+
+4.times do
+number = gets.chomp.to_i
+
+    if number.even?
+        pares.push(number)
+    else
+        impares.push(number)
+    end
+end
+
+puts "Números impares: #{impares}"
+puts "Números pares: #{pares}"
