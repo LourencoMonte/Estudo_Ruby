850404bfabf3fe26f6ba20bd836f19d12f0bd0ce
LourencoMonte <f.lourenco13aps@gmail.com>
2025-04-09 17:54:15 -0300

    feat: Adicionando código de soma de 2 números

---
+++ loresGem1/lib/soma2.rb
@@ -0,0 +1,15 @@
+def soma
+    puts "Digite o primeiro número"
+    number1 = gets.chomp.to_f
+
+    puts "Digite o segundo número"
+    number2 = gets.chomp.to_f
+
+    result = number1 + number2
+    puts "A soma é: #{result}"
+end
+
+#n1, n2 = coleta
+#resultado = soma(number1, number2)
+
+#puts "A soma e: #{resultado}"
