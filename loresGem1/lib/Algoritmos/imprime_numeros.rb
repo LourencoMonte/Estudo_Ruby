1aeeae2c7c4d2d51012a65529d70b4c868805c14
LourencoMonte <f.lourenco13aps@gmail.com>
2025-04-10 17:54:53 -0300

    refactor: adapta código para rodar em gem

--- loresGem1/lib/Algoritmos/imprime_numeros.rb
+++ loresGem1/lib/Algoritmos/imprime_numeros.rb
@@ -1,17 +1,19 @@
-impares = []
-pares = []
+def categoriza_numeros_par_e_impar
+    impares = []
+    pares = []
 
-puts 'Digite  4 números'
+    puts 'Digite  4 números'
 
-4.times do
-number = gets.chomp.to_i
+    4.times do
+    number = gets.chomp.to_i
 
-    if number.even?
-        pares.push(number)
-    else
-        impares.push(number)
+        if number.even?
+            pares.push(number)
+        else
+            impares.push(number)
+        end
     end
-end
 
-puts "Números impares: #{impares}"
-puts "Números pares: #{pares}"
+    puts "Números impares: #{impares}"
+    puts "Números pares: #{pares}"
+end
