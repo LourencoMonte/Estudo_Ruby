9095399159e2cfc035791617b179f202fce450d1
LourencoMonte <f.lourenco13aps@gmail.com>
2025-04-10 18:00:44 -0300

    refactor: adapta código para utilização em gem

--- loresGem1/lib/Algoritmos/numeros_primos.rb
+++ loresGem1/lib/Algoritmos/numeros_primos.rb
@@ -1,29 +1,31 @@
-#Método que analisa se number é primo
-def primo?(number)
-   return false if number < 2
+def verificador_numero_primo
+   #Método que analisa se number é primo
+   def primo?(number)
+      return false if number < 2
 
-   count = 0
-   for i in 1..number
-      if (number % i == 0)
-         count += 1
+      count = 0
+      for i in 1..number
+         if (number % i == 0)
+            count += 1
+         end
+         return false if count > 2
       end
-      return false if count > 2
+
+         true
    end
+ 
 
-      true
-end
+   #Captura do número
+   puts 'Escreva um número'
+   number = gets.chomp.to_i
 
-#Captura do número
-puts 'Escreva um número'
-number = gets.chomp.to_i
-
-#Chamada da função "primo?"
-if primo?(number)
-   puts "O número #{number} é primo"
-else
-   puts "O número #{number} não e primo"
-end
-
+   #Chamada da função "primo?"
+   if primo?(number)
+      puts "O número #{number} é primo"
+   else
+      puts "O número #{number} não e primo"
+   end
+end  
 =begin
 Algoritmo "Números primos"
 
