efe64a00a37bbe117c4d7b8211d3030cf8ed24b5
LourencoMonte <f.lourenco13aps@gmail.com>
2025-05-09 20:50:48 -0300

    refactor: adiciona funcionalidade para entrada de dados(valores/operação)

--- loresGem1/lib/Roteiro_de_Estudo_Pratico/a8_Ruby_avancado_1/Pratica/calculadora_lambda.rb
+++ loresGem1/lib/Roteiro_de_Estudo_Pratico/a8_Ruby_avancado_1/Pratica/calculadora_lambda.rb
@@ -2,11 +2,41 @@
 somar = -> (number1, number2) {number1 + number2}
 subtrair = -> (number1, number2) {number1 - number2}
 multiplicar = -> (number1, number2) {number1 * number2}
+dividir = -> (number1, number2) {number1 / number2}
 
-def calcular(number1, number2, operacao)
-    operacao.call(number1, number2)
+def dados_operacao
+    puts "Digie o primeiro número"
+    number1 = gets.chomp.to_f
+
+    puts "Digite o segundo número"
+    number2 = gets.chomp.to_f
+
+    puts "Digite o sinal da operação(+, -, *, /)."
+    operacao = gets.chomp
+
+    case operacao
+        when '+'
+            operacao_lambda = -> (a, b) {a + b}
+        when '-'
+            operacao_lambda = -> (a, b) {a - b}
+        when '*'
+            operacao_lambda = -> (a, b) {a * b}
+        when '/'
+            operacao_lambda = -> (a, b) {a / b}
+        else
+            puts 'Operação inválida'
+        return
+    end
+
+    return number1, number2, operacao_lambda
 end
 
-puts "Soma: #{somar.call(10,5)}"
-puts subtrair.call(20,8)
-puts multiplicar.call(5,5)
+def calcular(number1, number2, operacao)
+    resultado = operacao.call(number1, number2)
+    puts "O resultado é: #{resultado}"
+end
+
+number1, number2, operacao_lambda = dados_operacao
+if operacao_lambda
+  calcular(number1, number2, operacao_lambda)
+end
